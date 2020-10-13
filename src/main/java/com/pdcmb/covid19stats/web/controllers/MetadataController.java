5897f3045e2172d989b8cae7d77c812938db5cdf
Mateusz Ziomek <mateusz@pop-os.localdomain>
2020-10-13 17:18:21 +0200

    Added controllers for web api interface

---
+++ src/main/java/com/pdcmb/covid19stats/web/controllers/MetadataController.java
@@ -0,0 +1,13 @@
+package com.pdcmb.covid19stats.web.controllers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class MetadataController {
+    
+
+    
+
+}
